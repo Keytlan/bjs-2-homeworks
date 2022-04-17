c09cbc76ab723995ad6db2fabb45f0c9be50bfe8
Nikolay T <Keyaltor@gmail.com>
2022-04-17 21:23:00 +0400

    1st_task_complete

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,18 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr=[];
+  let d = (b**2)-(4*a*c);
+  
+  if (d > 0) {
+    let x1 = (-b + Math.sqrt(d) )/(2*a);
+    let x2 = (-b - Math.sqrt(d) )/(2*a);
+    
+    arr.push(x1, x2);
+  } else if (d == 0) {
+    x1 = -b/(2*a);
+    arr.push(x1);
+  } else if (d < 0){
+    arr = arr;
+  }
   return arr; // array
 }
 
